88ef903e295b347b2b038af05f62588d3ec72665
Yuriy <basov@restream.rt.ru>
2017-07-12 13:40:45 +0300

    init commit

---
+++ d_1_6_home/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+import example.Lists
+
+object Main extends App {
+  // TODO 2.A Using a Main Object
+  // on the package example in src/main/scala and select “New” - “Scala Object”
+  println(Lists.max(List(1,3,2)))
+}
